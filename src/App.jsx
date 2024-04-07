2b282affe40a54c4e2cf50bd9cea1af9087fca89
Zaman <zamanofficial1980@gmail.com>
2024-04-08 00:32:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Zaman</h1>
+        <h1>Hi Zaman</h1>
+      </div>
+   
+    </>
+  )
+}
+
+export default App
